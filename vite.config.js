31ddea9a0be7cf6b2ab3c3cd8cefa8b47f34a2ef
Nikhil Kudupudi <46317218+Nikhil-Kudupudi@users.noreply.github.com>
2025-07-25 17:53:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    base: '/Nikhil-Kudupudi.github.io/', // Replace with your repo name
+    // base: '/nikhil-kudupudi.github.io/', // Replace with your repo name
 })
